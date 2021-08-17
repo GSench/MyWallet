74cda9fc39706718d533371baa110f55833fa1a1
GSench <grishasenchenok@yandex.ru>
2021-08-17 17:22:51 +0300

    Splash Screen + Onboarding screen

---
+++ app/src/main/java/com/gsench/mywallet/GoogleSignInActivity.kt
@@ -0,0 +1,7 @@
+package com.gsench.mywallet
+
+import androidx.appcompat.app.AppCompatActivity
+
+class GoogleSignInActivity: AppCompatActivity() {
+
+}
--- app/src/main/java/com/gsench/mywallet/MainActivity.kt
+++ app/src/main/java/com/gsench/mywallet/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.gsench.mywallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.gms.auth.api.signin.*
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+
 
 class MainActivity : AppCompatActivity() {
+
+    var mGoogleSignInClient: GoogleSignInClient? = null
+    val RC_SIGN_IN = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_MyWallet)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
+
+    fun onGoogleBtnClick(v: View) {
+        signIn()
+    }
+
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient!!.getSignInIntent()
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account:GoogleSignInAccount = task.getResult(ApiException::class.java)
+            } catch (e: ApiException) {
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                Log.w("WARN", "signInResult:failed code=" + e.getStatusCode());
+            }
+
+        }
+    }
+
 }
